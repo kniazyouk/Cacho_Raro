474fa7c77a3f0a54359771ed6fd80f0a0f11cabc
George <kniazyouk@gmail.com>
2020-08-26 21:10:39 -0400

    second commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  dado1: number;
+  dado2: number;
+  dado3: number;
+  dado4: number;
+  resultado: string;
+
   constructor() {}
 
+  lanzar(){
+    this.dado1 = Math.floor(Math.random() * 3) + 1;
+    this.dado2 = Math.floor(Math.random() * 3) + 1;
+    this.dado3 = Math.floor(Math.random() * 3) + 1;
+    this.dado4 = Math.floor(Math.random() * 3) + 1;
+
+    if (this.dado1 === 0 && this.dado2 === 0 && this.dado3 === 0 && this.dado4 === 0) {
+      this.resultado = 'none';
+    } else if(this.dado1 === this.dado2 && this.dado1 === this.dado3 && this.dado1 === this.dado4) {
+      this.resultado = 'iguales';
+    } else {
+      this.resultado = 'no_iguales';
+    }
+
+  }
 }
